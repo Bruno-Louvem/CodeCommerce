4d861c14c3cc18731c7977f9279734bc4fdbf8ee
Bruno-Louvem <btlouvem@gmail.com>
2015-05-19 12:35:41 +0000

    Rotas dos CRUDs de Categorias e Produtos criadas

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -15,9 +15,38 @@ Route::get('/', 'WelcomeController@index');
 
 Route::get('home', 'HomeController@index');
 
-Route::get('admin/categories', 'AdminCategoriesController@index');
+//Inicio rotas categorias
 
+//Listagem de Categorias
+Route::get('admin/categories', 'AdminCategoriesController@index');
+//Form de cadastro
+Route::get('admin/categories/create', 'AdminCategoriesController@new');
+//Form de edição
+Route::get('admin/categories/edit', 'AdminCategoriesController@edit');
+//Action de cadastro
+Route::post('admin/categories','AdminCategoriesController@create');
+//Action de edição
+Route::put('admin/categories','AdminCategoriesController@update');
+//Action de deleção
+Route::delete('admin/categories','AdminCategoriesController@delete');
+//Fim rotas Categorias
+
+//Inicio rotas Produtos
+
+//Listagem de Produtos
 Route::get('admin/products', 'AdminProductsController@index');
+//Form de cadastro
+Route::get('admin/products/create', 'AdminProductsController@new');
+//Form de edição
+Route::get('admin/products/edit', 'AdminProductsController@edit');
+//Action de cadastro
+Route::post('admin/products','AdminProductsController@create');
+//Action de edição
+Route::put('admin/products','AdminProductsController@update');
+//Action de deleção
+Route::delete('admin/products','AdminProductsController@delete');
+//Fim rotas Produtos
+
 
 Route::controllers([
 	'auth' => 'Auth\AuthController',
